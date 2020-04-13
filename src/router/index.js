dbbaa418b3faff205a9534dc98d6128181c990d7
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-13 14:43:56 -0400

    Adicionando roteamento

--- src/router/index.js
+++ src/router/index.js
@@ -1,30 +1,33 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import EventList from '../views/EventList.vue'
+import EventShow from '../views/EventShow.vue'
 
-Vue.use(VueRouter);
+Vue.use(VueRouter)
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home
+    path: '/events',
+    name: 'events.list',
+    component: EventList
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+    path: '/events/:id',
+    name: 'events.show',
+    component: EventShow,
+    props: true
+  },
+  {
+    path: '/events/create',
+    name: 'events.create',
+    component: () => import('../views/EventCreate.vue')
   }
-];
+]
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes
-});
+})
 
-export default router;
+export default router
