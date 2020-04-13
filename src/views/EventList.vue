fcaf7d2ad2769333c52bbf903947822c6b6686d7
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-13 14:42:08 -0400

    Adicionando páginas de exibição

---
+++ src/views/EventList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Event list</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
+
+<script>
+import EventCard from '@/components/EventCard.vue'
+import EventService from '../services/EventService'
+
+export default {
+  created() {
+    EventService.getEvents().then(response => (this.events = response.data))
+  },
+  data() {
+    return {
+      events: []
+    }
+  },
+  components: {
+    EventCard
+  }
+}
+</script>
+
+<style></style>
