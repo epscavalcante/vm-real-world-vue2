5b6196a54bd5e9cc90607e14b8cf8ee5bf4b48ad
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-13 14:43:35 -0400

    Adicionando icones e melhorando componentes

--- src/main.js
+++ src/main.js
@@ -1,12 +1,36 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
+import BaseIcon from '@/components/BaseIcon.vue'
+import BaseInput from '@/components/BaseInput.vue'
+import BaseButton from '@/components/BaseButton.vue'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
+Vue.component('BaseIcon', BaseIcon)
+Vue.component('BaseButton', BaseButton)
+Vue.component('BaseInput', BaseInput)
+
+const requireComponent = require.context(
+  './components',
+  false,
+  /Base[A-Z]\w+\.(vue|js)$/
+)
+
+requireComponent.keys().forEach(fileName => {
+  const componentConfig = requireComponent(fileName)
+
+  const componentName = upperFirst(
+    camelCase(fileName.replace(/^\.\/(.*)\.\w+$/, '$1'))
+  )
+
+  Vue.component(componentName, componentConfig.default || componentConfig)
+})
 
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
