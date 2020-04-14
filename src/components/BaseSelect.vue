d0bf1b2dc989632bc48476dac7c173f6f5f6c7de
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-14 17:15:15 -0400

    Criando um form select global

---
+++ src/components/BaseSelect.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <label v-if="label">{{ label }}</label>
+    <select :value="value" @change="updateValue" v-bind="$attrs">
+      <option
+        v-for="option in options"
+        :selected="option === value"
+        :value="option"
+        :key="option"
+      >{{ option }}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  inheritAttrs: false,
+  props: {
+    options: {
+      // <-- new prop
+      type: Array,
+      required: true
+    },
+    value: [String, Number],
+    label: String
+  }
+}
+</script>
+
+<style lang="scss" scoped></style>
