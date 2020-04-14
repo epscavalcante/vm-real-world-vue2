b54c12b0a75e73bcea59f222b42b5f2a6c683d2a
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-14 17:15:30 -0400

    Definindo estrutura do form input global

--- src/components/BaseInput.vue
+++ src/components/BaseInput.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    Input
+    <label v-if="label">{{ label }}</label>
+    <input :value="value" @input="updateValue" v-bind="$attrs" />
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  inheritAttrs: false,
+  props: {
+    value: {
+      type: [String, Number],
+      required: false
+    },
+    label: {
+      type: String,
+      required: false
+    }
+  },
+  methods: {
+    updateValue(event) {
+      // <-- method triggered by input event
+      this.$emit('input', event.target.value)
+    }
+  }
+}
 </script>
 
 <style lang="scss" scoped></style>
