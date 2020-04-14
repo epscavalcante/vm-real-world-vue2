600772676b75adb48d6825512ac1193ffa2202ca
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-14 15:46:32 -0400

    Adicionando páginas de erro

---
+++ src/views/NetworkIssue.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Oh oh</h1>
+    <h3>You're has a problem with network</h3>
+    <router-link :to="{ name: 'events.list' }">List events</router-link>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
