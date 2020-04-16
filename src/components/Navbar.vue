3b1da2b34e90f7a364136e55ec0d171ddd9bac64
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-16 15:34:57 -0400

    configurando novo estilo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="nav" class="nav">
-    <router-link to="/" class="brand">VueJs Example Events</router-link>
-    <nav>
-      <router-link :to="{ name: 'events.list' }">List</router-link>|
-      <router-link :to="{ name: 'events.create' }">Create</router-link>
-    </nav>
+  <div id="nav">
+    <div class="container">
+      <router-link :to="{ name: 'home' }" class="brand">VueJs Example</router-link>
+      <nav>
+        <router-link :to="{ name: 'home' }">Home</router-link>|
+        <router-link :to="{ name: 'events.list' }">Events</router-link>|
+        <router-link :to="{ name: 'events.create' }">Logout</router-link>
+      </nav>
+    </div>
   </div>
 </template>
 
@@ -12,28 +15,57 @@
 export default {}
 </script>
 
-<style scoped>
-.nav {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  height: 60px;
+<style lang="scss">
+#nav {
+  width: 100%;
+  background: linear-gradient(to right, #16c0b0, #84cf6a);
+
+  .container {
+    min-height: 50px;
+    margin: 0 auto;
+    max-width: 650px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    .brand {
+      font-size: 1.5em;
+    }
+
+    &.nav-welcome {
+      // margin-left: auto;
+      // margin-right: 1rem;
+      color: white;
+    }
+    a {
+      font-weight: bold;
+      color: #2c3e50;
+      margin: auto 0.8em auto 0.4em;
+      text-decoration: none;
+      border-top: 2px solid transparent;
+      border-bottom: 2px solid transparent;
+    }
+    .router-link-exact-active {
+      color: white;
+      border-bottom: 2px solid #fff;
+    }
+  }
 }
-.nav > .brand {
-  font-family: 'Montserrat', sans-serif;
-  font-weight: 700;
-  font-size: 1.5em;
-  color: #39b982;
+
+button,
+.button {
+  margin-left: auto;
+  background: white;
   text-decoration: none;
+  color: #2c3e50;
+  &.router-link-active {
+    color: #2c3e50;
+  }
 }
-.nav .nav-item {
-  box-sizing: border-box;
-  margin: 0 5px;
-  color: rgba(0, 0, 0, 0.5);
-  text-decoration: none;
+.logoutButton {
+  cursor: pointer;
 }
-.nav .nav-item.router-link-exact-active {
-  color: #39b982;
-  border-bottom: solid 2px #39b982;
+.nav-welcome + button {
+  margin-left: 0;
 }
 </style>
