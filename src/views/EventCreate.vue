2f3476ce94a9c33bf96054a8d501bd21a3af1254
Eduardo Cavalcante <epscavalcant@gmail.com>
2020-04-16 10:32:20 -0400

    Validando criação do evento

--- src/views/EventCreate.vue
+++ src/views/EventCreate.vue
@@ -9,7 +9,13 @@
         placeholder="Add an event new category"
         :options="categories"
         class="field"
+        :class="{ error: $v.event.category.$error }"
+        @blur="$v.event.category.$touch()"
       />
+
+      <template v-if="$v.event.category.$error">
+        <p v-if="!$v.event.category.required" class="errorMessage">Category is required</p>
+      </template>
 
       <h3>Name & describe your event</h3>
 
@@ -19,7 +25,13 @@
         type="text"
         placeholder="Add an event new title"
         class="field"
+        :class="{ error: $v.event.title.$error }"
+        @blur="$v.event.title.$touch()"
       />
+
+      <template v-if="$v.event.title.$error">
+        <p v-if="!$v.event.title.required" class="errorMessage">Title is required</p>
+      </template>
 
       <BaseInput
         label="New description"
@@ -27,7 +39,13 @@
         type="text"
         placeholder="Add an event new desription"
         class="field"
+        :class="{ error: $v.event.description.$error }"
+        @blur="$v.event.description.$touch()"
       />
+
+      <template v-if="$v.event.description.$error">
+        <p v-if="!$v.event.description.required" class="errorMessage">Description is required</p>
+      </template>
 
       <h3>Where is your event?</h3>
 
@@ -37,14 +55,29 @@
         type="text"
         placeholder="Add an event new location"
         class="field"
+        :class="{ error: $v.event.location.$error }"
+        @blur="$v.event.location.$touch()"
       />
+
+      <template v-if="$v.event.location.$error">
+        <p v-if="!$v.event.location.required" class="errorMessage">Location is required</p>
+      </template>
 
       <h3>When is your event?</h3>
 
       <div class="field">
         <label>Date</label>
-        <datepicker v-model="event.date" placeholder="Select a date" />
+        <datepicker
+          v-model="event.date"
+          placeholder="Select a date"
+          :input-class="{ error: $v.event.date.$error }"
+          @opened="$v.event.date.$touch()"
+        />
       </div>
+
+      <template v-if="$v.event.date.$error">
+        <p v-if="!$v.event.date.required" class="errorMessage">Date is required</p>
+      </template>
 
       <BaseSelect
         v-model="event.time"
@@ -52,8 +85,17 @@
         placeholder="Add an event new time"
         :options="times"
         class="field"
+        :class="{ error: $v.event.time.$error }"
+        @blur="$v.event.time.$touch()"
       />
-      <BaseButton type="submit" buttonClass="-fill-gradient">Submit</BaseButton>
+
+      <template v-if="$v.event.time.$error">
+        <p v-if="!$v.event.time.required" class="errorMessage">Time is required</p>
+      </template>
+
+      <BaseButton type="submit" buttonClass="-fill-gradient" :disabled="$v.$anyError">Submit</BaseButton>
+
+      <p v-if="$v.$anyError" class="errorMessage">Resolve the errrors</p>
     </form>
   </div>
 </template>
@@ -63,6 +105,7 @@ import Datepicker from 'vuejs-datepicker'
 import { mapState } from 'vuex'
 import NProgress from 'nprogress'
 import BaseInput from '@/components/BaseInput.vue'
+import { required } from 'vuelidate/lib/validators'
 
 export default {
   data() {
@@ -73,6 +116,16 @@ export default {
     return {
       event: this.createFreshEvent(),
       times
+    }
+  },
+  validations: {
+    event: {
+      category: { required },
+      title: { required },
+      description: { required },
+      location: { required },
+      date: { required },
+      time: { required }
     }
   },
   computed: mapState(['categories']),
@@ -95,18 +148,21 @@ export default {
     },
 
     createEvent() {
-      NProgress.start()
-      this.$store
-        .dispatch('event/createEvent', this.event)
-        .then(() => {
-          this.$router.push({
-            name: 'events.show',
-            params: { id: this.event.id }
+      this.$v.$touch()
+      if (!this.$v.$invalid) {
+        NProgress.start()
+        this.$store
+          .dispatch('event/createEvent', this.event)
+          .then(() => {
+            this.$router.push({
+              name: 'events.show',
+              params: { id: this.event.id }
+            })
+            this.event = this.createFreshEventObject()
           })
-          this.event = this.createFreshEventObject()
-        })
-        .catch(() => {})
-        .finally(() => NProgress.done())
+          .catch(() => {})
+          .finally(() => NProgress.done())
+      }
     }
   },
   components: {
